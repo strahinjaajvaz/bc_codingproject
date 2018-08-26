98b143b6eaf32af17a77e01dedaad44af93f2892
Strahinja Ajvaz <strahinja@dreamwalk.com>
2018-08-26 15:43:36 +1000

    added path to server so it returns the index.html page when requested

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(`${__dirname}/client/dist/`));
+
+app.get("*", (req, res) => {
+  res.sendFile(`${__dirname}/client/dist/index.html`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
