c2d76168790cd4d3f5449a9aa6d7ef4e9dcdc625
Strahinja Ajvaz <strahinja@dreamwalk.com>
2018-08-26 14:15:53 +1000

    added webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,21 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const 
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
